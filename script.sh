2e40c3fc2da3c82752cae47717e2cd2ec7bf5806
zhao qiqi <bme_ritter@foxmail.com>
2017-03-04 02:19:41 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-rm -rf collection-operation
-
-git clone https://github.com/bmeritter/collection-operation.git
-
-rm -rf  spec
-
-mv collection-operation/spec ./
-
 npm i
 
 npm test
